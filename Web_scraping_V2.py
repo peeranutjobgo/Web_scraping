ed4fd86ae1793acd4ecdd56f29241e608a8eaee2
peeranutjobgo@gmail.com <peeranutjobgo@gmail.com>
2024-03-25 09:17:22 +0700

    mod code

--- Web_scraping_V2.py
+++ Web_scraping_V2.py
@@ -40,85 +40,85 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 url = "https://dictionary.orst.go.th/lookup_domain.php"
 driver.get(url)
 
-# wait = WebDriverWait(driver, 60)
-# word_data = pd.DataFrame(columns=["หมวดอักษร", "คำ", "ความหมาย"])
-
-# # word_count =  pd.DataFrame(columns=["หมวดอักษร", "หน้าทั้งหมด", "หน้าสุดท้ายที่ต้องการดึง"])
-# # thai_alphabet = ['ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ต', 'ถ', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
-# alphabet_buttons_1 =  ['ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ต', 'ถ']
-# print(alphabet_buttons_1)
-
-# for alphabet in alphabet_buttons_1:
-#     last_page = last_page_intext = 0
-#     button_xpath = f"//button[@onclick=\"getwordlist('{alphabet}')\"]"
-#     wait.until(EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
-#     print(f"Processing alphabet: {alphabet}")
-#     # ------------------------------------------------------------------------------------------------ 
-#     # รับจำนวนหน้าทั้งหมดสำหรับหมวดอักษรนี้
-#     pages_xpath = "//span[@id='spnPage']"
-#     pages_text = wait.until(EC.presence_of_element_located((By.XPATH, pages_xpath))).text
-#     # print(pages_text)
-#     last_page_intext = extract_total_pages(pages_text)
-#     print(f"หน้าสุดท้ายในหมวดอักษรนี้ : {last_page_intext}")
-#     # ------------------------------------------------------------------------------------------------ 
-#     # ถ้าจำนวนหน้าน้อยกว่า 10 ให้ตั้งค่าให้ตามจำนวนหน้าที่มีจริง
-#     last_page = min(last_page_intext, 10)
-#     print(last_page)
-#     print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
-#     # new_row_word_count = pd.DataFrame({"หมวดอักษร": [alphabet], "หน้าทั้งหมด": [last_page_intext], "หน้าสุดท้ายที่ต้องการดึง": [last_page]})
-#     # word_count = pd.concat([word_count, new_row_word_count], ignore_index=True)
-#     time.sleep(5)
-#     # ------------------------------------------------------------------------------------------------ 
-    
-#     last_page += 1
-#     for page in range(1, last_page):  # วน loop จนถึงหน้าที่ 10
-#         # if page == last_page : break
-#         print(f"Processing page: {page}")
-#         print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
-#         word_elements = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@class,'list-group-item') and contains(@onclick,'lookupWord')]")))
-        
-#         for index in range(len(word_elements)):
-#             word_elements = wait.until(EC.presence_of_all_elements_located(
-#             (By.XPATH, "//button[contains(@class,'list-group-item') and contains(@onclick,'lookupWord')]")))
-#             if index < len(word_elements):
-#                 # word_button = word_elements[index]
-#         # for index in range(len(word_elements)):
-#         #     word_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//button[contains(@class,'list-group-item') and contains(@onclick,'lookupWord')]")))
-#                 word_button = word_elements[index]
-#                 word = word_button.text
-#                 print(f"this word : {word}")
-#                 word_button.click()
-#                 time.sleep(5)
-#                 print("------------------------------------------------------------------------------------")
-#                 meaning_xpath = '//*[@id="r_lookup"]/div/div[2]'
-#                 meaning = wait.until(EC.presence_of_element_located((By.XPATH, meaning_xpath))).text
-#                 print(f"this word meaning : {meaning}")
-#                 print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
-#                 new_row = pd.DataFrame({"หมวดอักษร": [alphabet], "คำ": [word], "ความหมาย": [meaning]})
-#                 word_data = pd.concat([word_data, new_row], ignore_index=True)
-#                 time.sleep(5)
-#                 # time.sleep()
-
-#         # บันทึกข้อมูลลงไฟล์
-#         word_data.to_csv("word_data.csv", index=False, encoding='utf-8-sig')
-#         word_data.to_json("word_data.json", orient='records', force_ascii=False)
-        
-#         print(f"Processing page: {page} in {last_page - 1}")
-#         # if page == last_page + 1 : break
-        
-#         if page < last_page:
-#             next_page_button = wait.until(EC.element_to_be_clickable((By.ID, "nextpage")))
-#             next_page_button.click()
-#             time.sleep(5)
-
-
-wait = WebDriverWait(driver, 30)
+wait = WebDriverWait(driver, 60)
 word_data = pd.DataFrame(columns=["หมวดอักษร", "คำ", "ความหมาย"])
 
-# print(f"Processing is susussfull in set one,go to set two") 
-# print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
-# alphabet_buttons_2 = ['ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
-alphabet_buttons_2 = ['ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
+# word_count =  pd.DataFrame(columns=["หมวดอักษร", "หน้าทั้งหมด", "หน้าสุดท้ายที่ต้องการดึง"])
+# thai_alphabet = ['ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ต', 'ถ', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
+alphabet_buttons_1 =  ['ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ต', 'ถ']
+print(alphabet_buttons_1)
+
+for alphabet in alphabet_buttons_1:
+    last_page = last_page_intext = 0
+    button_xpath = f"//button[@onclick=\"getwordlist('{alphabet}')\"]"
+    wait.until(EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
+    print(f"Processing alphabet: {alphabet}")
+    # ------------------------------------------------------------------------------------------------ 
+    # รับจำนวนหน้าทั้งหมดสำหรับหมวดอักษรนี้
+    pages_xpath = "//span[@id='spnPage']"
+    pages_text = wait.until(EC.presence_of_element_located((By.XPATH, pages_xpath))).text
+    # print(pages_text)
+    last_page_intext = extract_total_pages(pages_text)
+    print(f"หน้าสุดท้ายในหมวดอักษรนี้ : {last_page_intext}")
+    # ------------------------------------------------------------------------------------------------ 
+    # ถ้าจำนวนหน้าน้อยกว่า 10 ให้ตั้งค่าให้ตามจำนวนหน้าที่มีจริง
+    last_page = min(last_page_intext, 10)
+    print(last_page)
+    print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
+    # new_row_word_count = pd.DataFrame({"หมวดอักษร": [alphabet], "หน้าทั้งหมด": [last_page_intext], "หน้าสุดท้ายที่ต้องการดึง": [last_page]})
+    # word_count = pd.concat([word_count, new_row_word_count], ignore_index=True)
+    time.sleep(5)
+    # ------------------------------------------------------------------------------------------------ 
+    
+    last_page += 1
+    for page in range(1, last_page):  # วน loop จนถึงหน้าที่ 10
+        # if page == last_page : break
+        print(f"Processing page: {page}")
+        print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
+        word_elements = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@class,'list-group-item') and contains(@onclick,'lookupWord')]")))
+        
+        for index in range(len(word_elements)):
+            word_elements = wait.until(EC.presence_of_all_elements_located(
+            (By.XPATH, "//button[contains(@class,'list-group-item') and contains(@onclick,'lookupWord')]")))
+            if index < len(word_elements):
+                # word_button = word_elements[index]
+        # for index in range(len(word_elements)):
+        #     word_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//button[contains(@class,'list-group-item') and contains(@onclick,'lookupWord')]")))
+                word_button = word_elements[index]
+                word = word_button.text
+                print(f"this word : {word}")
+                word_button.click()
+                time.sleep(5)
+                print("------------------------------------------------------------------------------------")
+                meaning_xpath = '//*[@id="r_lookup"]/div/div[2]'
+                meaning = wait.until(EC.presence_of_element_located((By.XPATH, meaning_xpath))).text
+                print(f"this word meaning : {meaning}")
+                print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
+                new_row = pd.DataFrame({"หมวดอักษร": [alphabet], "คำ": [word], "ความหมาย": [meaning]})
+                word_data = pd.concat([word_data, new_row], ignore_index=True)
+                time.sleep(5)
+                # time.sleep()
+
+        # บันทึกข้อมูลลงไฟล์
+        word_data.to_csv("word_data.csv", index=False, encoding='utf-8-sig')
+        word_data.to_json("word_data.json", orient='records', force_ascii=False)
+        
+        print(f"Processing page: {page} in {last_page - 1}")
+        # if page == last_page + 1 : break
+        
+        if page < last_page:
+            next_page_button = wait.until(EC.element_to_be_clickable((By.ID, "nextpage")))
+            next_page_button.click()
+            time.sleep(5)
+
+
+# wait = WebDriverWait(driver, 30)
+# word_data = pd.DataFrame(columns=["หมวดอักษร", "คำ", "ความหมาย"])
+
+print(f"Processing is susussfull in set one,go to set two") 
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
+alphabet_buttons_2 = ['ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
+# alphabet_buttons_2 = ['ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
 print(alphabet_buttons_2)
 
 for alphabet in alphabet_buttons_2:
